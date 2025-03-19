b8dbe160586b96deb59c779f04427439e3ac7d86
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2025-03-19 16:29:20 -0400

    had to add text to humanpy for gptzero to check it

--- humanPy.py
+++ humanPy.py
@@ -1,6 +1,8 @@
 #Bubble Sort
 def bubbleSort(arr):
+    #amount of times the loop will run
     for i in range(len(arr) - 1):
+        #start at the beginning and check to the end
         for j in range(0, len(arr) - 1 - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
